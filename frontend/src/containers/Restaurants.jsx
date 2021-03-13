33a18a2bd52c6a26ae1951ec2e06c2d06a295e54
mrmtsu <81202muramatsu@gmail.com>
2021-03-13 23:58:09 +0900

    first commit

---
+++ frontend/src/containers/Restaurants.jsx
@@ -0,0 +1,47 @@
+import React, { Fragment, useEffect } from 'react';
+import styled from 'styled-components';
+
+// apis
+import { fetchRestaurants } from '../apis/restaurants';
+
+// images
+import MainLogo from '../images/1.jpg';
+import MainCoverImage from '../images/2.jpg';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
+
+export const Restaurants = () => {
+  useEffect(() => {
+    fetchRestaurants()
+    .then((data) =>
+      console.log(data)
+    )
+  }, [])
+
+  return (
+    <Fragment>
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+    </Fragment>
+  )
+}
